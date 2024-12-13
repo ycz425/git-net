425ba417df60d79d909fb52ffd1d612e619a3306
John Zhang <23zhanyuc@gmail.com>
2024-12-13 14:35:41 -0500

    Switch persistent memory from JSON file to SQLite

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -2,54 +2,103 @@ import requests
 import json
 import time
 import os
+import sqlite3
 from dotenv import load_dotenv
 
 
-def fetch_repositories(num: int, token: str) -> list[dict]:
-    URL = 'https://api.github.com/repositories'
-    params = {'since': 0}
+def fetch_data(num_repo: int, token: str, con: sqlite3.Connection):
+    URL = 'https://api.github.com/search/repositories'
+    params = {'q': 'stars:>1000 is:public', 'sort': 'stars', 'order': 'desc', 'per_page': 100, 'page': 1}
     headers = {'Authorization': f'Bearer {token}'}
 
-    data = []
-    while len(data) < num:
-        response = requests.get(URL, params, headers=headers).json()
+    count = 0
 
-        if type(response) != list:
-            print('Sleeping...')
-            time.sleep(3600)
+    cur = con.cursor()
+    while count < num_repo:
+        response = requests.get(URL, params, headers=headers)
+
+        if response.status_code != 200:
+            print(f'Sleeping... (in fetch_data)')
+            time.sleep(70)
             continue
 
-        for repo in response:
-            data.append(repo)
-            print(f'Fetched repository {len(data)}/{num}')
-            if len(data) >= num:
+        response = response.json()
+
+        for repo in response['items']:
+            cur.execute('INSERT INTO repositories VALUES (?, ?)', (repo['id'], json.dumps(repo)))
+            success = _fetch_contributions(repo, token, con)
+            if not success:
+                cur.execute('DELETE FROM repositories WHERE id = ?', (repo['id'],))
+                print(f'Failed to fetch data for {repo['full_name']}')
+            else:
+                count += 1
+                print(f'Fetched repository {count}/{num_repo}')
+            
+            if count >= num_repo:
                 break
         
-        params['since'] = data[-1]['id']
+        params['page'] += 1
 
-    return data
+    con.commit()
 
 
-def add_contributors(data: list[dict], token: str) -> None:
+def _fetch_contributions(repo: dict, token: str, con: sqlite3.Connection) -> bool:
     headers = {'Authorization': f'Bearer {token}'}
-    for index, repo in enumerate(data):
-        response = requests.get(repo['contributors_url'], headers=headers).json()
+    params = {'anon': 0, 'per_page': 100, 'page': 1}
+    cur = con.cursor()
 
-        while type(response) != list:
-            print('Sleeping...')
-            time.sleep(3600)
-            response = requests.get(repo['contributors_url'], headers=headers)
+    count = 0
+
+    while True:
+        response = requests.get(repo['contributors_url'], params, headers=headers)
+        if response.status_code != 200 and response.json()['message'] == 'The history or contributor list is too large to list contributors for this repository via the API.':
+            return False
         
-        repo['contributors'] = response
-        print(f'Add contributors for repository {index + 1}/{len(data)}')
+        if response.status_code != 200:
+            print(f'Sleeping... (in _fetch_contributions)')
+            time.sleep(300)
+            continue
+    
+        response = response.json()
+
+        for contributor in response:
+            cur.execute(
+                'INSERT INTO contributions (repo_id, body) VALUES (?, ?)',
+                (repo['id'], json.dumps(contributor))
+            )
+            count += 1
+
+        params['page'] += 1
+
+        if count >= 500 or len(response) == 0:
+            break
+
+    return True
+
+        
+def create_tables(con: sqlite3.Connection):
+    cur = con.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS repositories(
+                id INTEGER PRIMARY KEY,
+                body TEXT NOT NULL
+        )
+    ''')
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS contributions(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            repo_id INTEGER NOT NULL,
+            body TEXT NOT NULL,
+            FOREIGN KEY (repo_id) REFERENCES repositories (id) ON DELETE CASCADE
+        )
+    ''')
 
 
 if __name__ == '__main__':
+    response = requests.get('https://api.github.com/repositories')
     load_dotenv()
     TOKEN = os.getenv('GITHUB_FINE_GRAINED_ACCESS_TOKEN')
-    data = fetch_repositories(50000, TOKEN)
-    add_contributors(data, TOKEN)
-
-    with open('data/raw/repositories_raw_data.json', 'w') as file:
-        json.dump(data, file, indent=4)
-
+    
+    con = sqlite3.connect('data/raw_data.db')
+    create_tables(con)
+    fetch_data(10000, TOKEN, con)

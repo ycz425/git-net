e1b453ee12b82ca6b4b01bed0432141516529616
John Zhang <23zhanyuc@gmail.com>
2024-12-20 13:07:16 -0500

    Add stargazers logic to data processing script

--- data/process_data.py
+++ data/process_data.py
@@ -2,14 +2,37 @@ import sqlite3
 import json
 
 
-def process_data(con_processed: sqlite3.Connection, con_raw: sqlite3.Connection) -> None:
+def process_repos(con_processed: sqlite3.Connection, con_raw: sqlite3.Connection) -> None:
     cur_processed = con_processed.cursor()
     cur_raw = con_raw.cursor()
 
     cur_raw.execute('SELECT * FROM repositories')
     for repo in cur_raw.fetchall():
         body = json.loads(repo[2])
-        cur_processed.execute('INSERT INTO repositories VALUES (?, ?, ?, ?)', (repo[0], repo[1], body['full_name'], body['stargazers_count']))
+        cur_processed.execute('INSERT OR IGNORE INTO repositories VALUES (?, ?, ?, ?)', (repo[0], repo[1], body['full_name'], body['stargazers_count']))
+
+    con_processed.commit()
+
+
+def process_users(con_processed: sqlite3.Connection, con_raw: sqlite3.Connection) -> None:
+    cur_processed = con_processed.cursor()
+    cur_raw = con_raw.cursor()
+
+    cur_raw.execute('SELECT id, body FROM stargazers WHERE id IN(SELECT id FROM stargazers GROUP BY id HAVING COUNT(id) >= 3)')
+    for user in cur_raw.fetchall():
+        body = json.loads(user[1])
+        cur_processed.execute('INSERT OR IGNORE INTO users VALUES (?, ?, ?)', (user[0], body['login'], body['avatar_url']))
+       
+    con_processed.commit()
+
+
+def process_stars(con_processed: sqlite3.Connection, con_raw: sqlite3.Connection) -> None:
+    cur_processed = con_processed.cursor()
+    cur_raw = con_raw.cursor()
+
+    cur_raw.execute('SELECT id, repo_id FROM stargazers WHERE id IN(SELECT id FROM stargazers GROUP BY id HAVING COUNT(id) >= 3)')
+    for star in cur_raw.fetchall():
+        cur_processed.execute('INSERT OR IGNORE INTO stars VALUES (?, ?)', (star[0], star[1]))
 
     con_processed.commit()
 
@@ -27,6 +50,24 @@ def create_table(con: sqlite3.Connection) -> None:
         )
     ''')
 
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY,
+            login TEXT NOT NULL,
+            avatar_url TEXT NOT NULL
+        )
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS stars(
+            user_id,
+            repo_id,
+            PRIMARY KEY (user_id, repo_id),
+            FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+            FOREIGN KEY (repo_id) REFERENCES repositories (id) ON DELETE CASCADE
+        )
+    ''')
+
     con.commit()
 
 
@@ -34,4 +75,6 @@ if __name__ == '__main__':
     con_processed = sqlite3.connect('data/processed_data.db')
     con_raw = sqlite3.connect('data/raw_data.db')
     create_table(con_processed)
-    process_data(con_processed, con_raw)
+    process_repos(con_processed, con_raw)
+    process_users(con_processed, con_raw)
+    process_stars(con_processed, con_raw)

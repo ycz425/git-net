6866eb36e100c47b0533d243a7a525a64557f36b
John Zhang <23zhanyuc@gmail.com>
2024-12-20 15:19:03 -0500

    Add data access functions for users and stars

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import src.graph as graph
 if __name__ == '__main__':
     repos = da.get_repositories()
     forks = da.get_forks()
-    nodes = [(repo['id'], {'name': repo['full_name'], 'stars': repo['stargazers_count']}) for repo in repos]
+    users = da.get_users()
+    stars = da.get_stars()
+    nodes = [(repo['id'], {'type': 'repo'}) for repo in repos]
+    nodes += [(user['id'], {'type': 'user'}) for user in users]
     edges = [(fork['id'], fork['parent_id']) for fork in forks if fork['parent_id'] is not None]
+    edges += [(star['user_id'], star['repo_id']) for star in stars]
     G = graph.construct(nodes, edges)
     render_graph(G)

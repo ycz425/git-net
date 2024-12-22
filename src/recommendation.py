919ff1934a0546978429e329dd1f467ad7dc2b78
John Zhang <23zhanyuc@gmail.com>
2024-12-22 11:43:20 -0500

    Implement hybrid recommendation logic

--- src/recommendation.py
+++ src/recommendation.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import numpy as np
 from numpy.linalg import norm
-import random
 
 
 def recommend_repos(G: nx.Graph, target_user: int, num: int) -> list[int]:
@@ -17,18 +16,30 @@ def recommend_repos(G: nx.Graph, target_user: int, num: int) -> list[int]:
     target_user_index = users.index(target_user)
     target_user_vector = user_repo_matrix[target_user_index]
     target_user_vector_norm = norm(target_user_vector)
-    cosine_similarities = np.zeros(len(users))
+    user_similarities = np.zeros(len(users))
     for i in range(len(users)):
-        cosine_similarities[i] = np.dot(user_repo_matrix[i], target_user_vector) / (norm(user_repo_matrix[i]) * target_user_vector_norm)
+        user_similarities[i] = np.dot(user_repo_matrix[i], target_user_vector) / (norm(user_repo_matrix[i]) * target_user_vector_norm)
 
-    top_user_indices = np.argsort(cosine_similarities)[::-1]
+    top_user_indices = np.argsort(user_similarities)
     top_user_indices = top_user_indices[top_user_indices != target_user_index]
 
-    results = set()
+    user_to_user_scores = np.zeros(len(repos))
     for user_index in top_user_indices:
         for i in range(len(repos)):
             if user_repo_matrix[user_index, i] == 1 and user_repo_matrix[target_user_index, i] != 1:
-                results.add(repos[i])
+                user_to_user_scores[i] = user_similarities[user_index]
+
+    repo_to_repo_scores = np.zeros(len(repos))
+    for i in range(len(repos)):
+        if user_repo_matrix[target_user_index, i] == 1:
+            for neighbor in G.neighbors(repos[i]):
+                if G.nodes[neighbor]['type'] == 'repo':
+                    neighbor_index = repos.index(neighbor)
+                    if user_repo_matrix[target_user_index, neighbor_index] != 1:
+                        repo_to_repo_scores[neighbor_index] = 1
     
-    return random.sample(results, num)
+    scores = 0.7 * user_to_user_scores + 0.3 * repo_to_repo_scores
+    top_score_indices = np.argsort(scores)[::-1][:num]
+    
+    return [repos[index] for index in top_score_indices]
     

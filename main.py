c56ea1e864e04aa43bb59498d8b2530a9a79dcc6
John Zhang <23zhanyuc@gmail.com>
2024-12-23 14:16:00 -0500

    Add plotly menu options to include/exclude users

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import data.scripts.data_access as da
 from src.render import render_graph
-import src.graph as graph
-from src.recommendation import recommend_repos
 
 
 if __name__ == '__main__':
-    render_graph(include_users=True, show_communities=True)
+    render_graph(show_communities=False)
     
--- src/render.py
+++ src/render.py
@@ -6,10 +6,68 @@ import plotly.graph_objects as go
 from networkx.algorithms.community import louvain_communities
 import src.graph as graph
 
-def render_graph(G: nx.Graph = None, include_users=False, show_communities=False) -> None:
-    if not G:
-        with open('data/graphs/repos_and_users.pkl' if include_users else 'data/graphs/repos.pkl', 'rb') as file:
-            G = pickle.load(file)
+
+def _create_node_trace(G: nx.Graph, type: str) -> go.Scatter:
+    node_x = []
+    node_y = []
+    nodes = []
+    for node in G.nodes():
+        if G.nodes[node]['type'] == type:
+            x, y = G.nodes[node]['pos']
+            nodes.append(node)
+            node_x.append(x)
+            node_y.append(y)
+
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        text=[f'Repo: {G.nodes[node]['full_name']}' if G.nodes[node]['type'] == 'repo' else f'User: {G.nodes[node]['login']}' for node in nodes],
+        marker=dict(
+            size=10,
+            color=[G.nodes[node]['color'] for node in nodes],
+            line=dict(
+                color='white',
+                width=1
+            )
+        )
+    )
+
+    return node_trace
+
+
+def _create_edge_trace(G: nx.Graph, type: str) -> go.Scatter:
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        if (
+            (type == 'fork' and edge[0].startswith('repo') and edge[1].startswith('repo')) or
+            (type == 'star' and (edge[0].startswith('user') or edge[1].startswith('user')))
+        ):
+            x0, y0 = G.nodes[edge[0]]['pos']
+            x1, y1 = G.nodes[edge[1]]['pos']
+            edge_x.append(x0)
+            edge_x.append(x1)
+            edge_x.append(None)
+            edge_y.append(y0)
+            edge_y.append(y1)
+            edge_y.append(None)
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        mode='lines',
+        hoverinfo='none',
+        line=dict(width=0.5, color='#ffffff')
+    )
+
+    return edge_trace
+
+
+
+def render_graph(show_communities=False) -> None:
+    with open('data/graphs/repos_and_users.pkl', 'rb') as file:
+        G = pickle.load(file)
 
     groups = louvain_communities(G) if show_communities else [set(g) for g in graph.connected_repos(G)]
     group_colors = [mcolors.to_hex(color) for color in distinctipy.get_colors(len(groups))]
@@ -22,49 +80,13 @@ def render_graph(G: nx.Graph = None, include_users=False, show_communities=False
                     G.nodes[node]['color'] = group_colors[i]
                     break
 
-    edge_x = []
-    edge_y = []
-    for edge in G.edges():
-        x0, y0 = G.nodes[edge[0]]['pos']
-        x1, y1 = G.nodes[edge[1]]['pos']
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
-
-    edge_trace = go.Scatter(
-        x=edge_x, y=edge_y,
-        mode='lines',
-        hoverinfo='none',
-        line=dict(width=0.5, color='#ffffff')
-    )
-
-    node_x = []
-    node_y = []
-    for node in G.nodes():
-        x, y = G.nodes[node]['pos']
-        node_x.append(x)
-        node_y.append(y)
-
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers',
-        hoverinfo='text',
-        text=[f'Repo: {G.nodes[node]['full_name']}' if G.nodes[node]['type'] == 'repo' else f'User: {G.nodes[node]['login']}' for node in G.nodes()],
-        marker=dict(
-            size=10,
-            color=[G.nodes[node]['color'] for node in G.nodes()],
-            line=dict(
-                color='white',
-                width=1
-            )
-        )
-    )
+    repo_node_trace = _create_node_trace(G, 'repo')
+    user_node_trace = _create_node_trace(G, 'user')
+    fork_edge_trace = _create_edge_trace(G, 'fork')
+    star_edge_trace = _create_edge_trace(G, 'star')
 
     fig = go.Figure(
-        data=[edge_trace, node_trace],
+        data=[fork_edge_trace, star_edge_trace, user_node_trace, repo_node_trace],
         layout=go.Layout(
             title=dict(
                 text="GitNet",
@@ -81,7 +103,31 @@ def render_graph(G: nx.Graph = None, include_users=False, show_communities=False
             hovermode='closest',
             margin=dict(b=20,l=5,r=5,t=40),
             xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            updatemenus=[
+                dict(
+                    buttons=list([
+                        dict(
+                            args=[{"visible": [True, True, True, True]}],
+                            label='Repositories + Users',
+                            method='update'
+
+                        ),
+                        dict(
+                            args=[{"visible": [True, False, False, True]}],
+                            label='Repositories',
+                            method='update'
+                        )
+                    ]),
+                    direction='down',
+                    pad={"r": 10, "t": 10},
+                    showactive=True,
+                    x=0.15,
+                    xanchor="left",
+                    y=1.05,
+                    yanchor="top"
+                )
+            ]
         )
     )
 
